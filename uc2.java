8136d776543d02b98b6e0e8287e8c5c58018969d
pm5120-alt <pm5120@srmist.edu.in>
2026-02-17 09:39:41 +0530

    Add uc2.java to print a pattern

--- uc2.java
+++ uc2.java
@@ -1,1 +1,22 @@
+package github.pm5120;
 
+public class uc2 {
+    public static void main(String[] args) {
+
+        
+        System.out.println(" *****   *****   *****   ***** ");
+        System.out.println("*     * *     * *     * *      ");
+
+        
+        System.out.println("*     * *     *  ****** ******* ");
+
+        
+        System.out.println("*     * *     * *             *");
+
+        
+        System.out.println("*     * *     * *             *");
+
+        
+        System.out.println(" *****   *****  *        ***** ");
+    }
+}
