479dad9f4cb5e26c5571ff7ba64574cd5c227bb1
pm5120-alt <pm5120@srmist.edu.in>
2026-02-17 10:09:49 +0530

    Add uc3.java to print asterisk pattern
    This Java program prints a pattern using asterisks and spaces, creating a visual representation through multiple lines of formatted output.

---
+++ uc3.java
@@ -0,0 +1,45 @@
+package github.pm5120;
+
+public class uc3 {
+
+    public static void main(String[] args) {
+
+        System.out.println(String.join(" ",
+                "  ***   ", "  ***   ", "  **** ", "      ***** "
+        ));
+
+        System.out.println(String.join(" ",
+                "**   **", "**     **", " **   **", "  **     "
+        ));
+          System.out.println(String.join(" ",
+                "**   **", "**     **", " **   **", " **     "
+        ));
+
+        System.out.println(String.join(" ",
+                "**   **", "**     **", " **   **", "  **       "
+        ));
+
+        System.out.println(String.join(" ",
+                "**   **", "**     **", " ***** ", "     *** "
+        ));
+
+        System.out.println(String.join(" ",
+                "**   **", "**     **", " **     ", "       **"
+        ));
+
+        System.out.println(String.join(" ",
+                "**   **", "**     **", " **   ", "          **"
+                
+        ));
+                System.out.println(String.join(" ",
+                "**   **", "**     **", " **   ", "        **"
+        ));
+
+        System.out.println(String.join(" ",
+                "  ***   ", "  ***   ", " **     ", " ***** "
+        ));
+    }
+}
+
+
+
