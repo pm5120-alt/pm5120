4230da7a7770394ddeeafa4ab5ffc44a07677aea
pm5120-alt <pm5120@srmist.edu.in>
2026-02-23 18:29:20 +0530

    Implement star pattern printing in OOPSJoin class
    Added a Java program that prints a star pattern using String.join.

--- uc5.java
+++ uc5.java
@@ -1,1 +1,38 @@
+package github.pm5120;
 
+public class OOPSJoin {
+    public static void main(String[] args) {
+
+        String[] pattern = {
+                String.join("  ",
+                        " *** ",
+                        " *** ",
+                        "**** ",
+                        " ****"),
+                String.join("  ",
+                        "*   *",
+                        "*   *",
+                        "*   *",
+                        "*    "),
+                String.join("  ",
+                        "*   *",
+                        "*   *",
+                        "**** ",
+                        " *** "),
+                String.join("  ",
+                        "*   *",
+                        "*   *",
+                        "*    ",
+                        "    *"),
+                String.join("  ",
+                        " *** ",
+                        " *** ",
+                        "*    ",
+                        "**** ")
+        };
+
+        for (String row : pattern) {
+            System.out.println(row);
+        }
+    }
+}
