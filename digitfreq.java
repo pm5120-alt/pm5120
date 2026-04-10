27cef2b6cf1c2555497569b117e5996742d9bd45
pm5120-alt <pm5120@srmist.edu.in>
2026-04-10 17:27:45 +0530

    Add DigitFrequency class to count digit occurrences
    Implement a program to count digit frequencies in an integer input.

---
+++ digitfreq.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class DigitFrequency {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        int[] freq = new int[10];
+
+        while (num > 0) {
+            int d = num % 10;
+            freq[d]++;
+            num /= 10;
+        }
+
+        for (int i = 0; i < 10; i++)
+            if (freq[i] > 0)
+                System.out.println(i + " -> " + freq[i]);
+    }
+}
